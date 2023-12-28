93b202853d3ed386ba39dc733d16187dfbceb173
zac <zacwilliams11@hotmail.com>
2023-12-29 00:06:29 +1100

    Fixed bug so that the csv is read properly and sql and properly insert into database

--- src/main/java/com/source/readData.java
+++ src/main/java/com/source/readData.java
@@ -18,9 +18,6 @@ public class readData {
     	String file = "books.csv";
 		readCSV(file);
 		add_to_db(bookList);
-		//readData t = new readData();
-		//ArrayList<Book> bookList = t.readCSV(file);
-		//System.out.println(bookList.get(0).getIsbn());
 	}
 
 	public static void readCSV(String file) { 
@@ -28,49 +25,45 @@ public class readData {
 			// Create an object of file reader 
 			// class with CSV file as a parameter. 
 			FileReader filereader = new FileReader(file); 
+			
+			CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
 
-			// create csvReader object and skip first Line 
 			CSVReader csvReader = new CSVReaderBuilder(filereader) 
-								.withSkipLines(1) 
-								.build(); 
-			List<String[]> allData = csvReader.readAll(); 
+											.withSkipLines(1)
+											.withCSVParser(parser) 
+											.build(); 
+			
+			List<String[]> allData = csvReader.readAll();
 
-			//ArrayList<Book> bookList = new ArrayList<Book>();
+			for (String[] tempBook : allData) {  
 
-			// Create ArrayList of Book objects
-			for (String[] row : allData) { 
-				for (String cell : row) { 
-					// Remove quotes
-					String cleanCell = cell.replace("\"", "");
-					String[] tempBook = cleanCell.split(";", -1);
+				String[] tempUrls;
+				tempUrls = new String[] {tempBook[5], tempBook[6], tempBook[7]};
 
-					String[] tempUrls;
-					tempUrls = new String[] {tempBook[5], tempBook[6], tempBook[7]};
-
-					Book b = new Book(tempBook[0], tempBook[1], tempBook[2], tempBook[3], tempBook[4], tempUrls);
+				Book b = new Book(tempBook[0], tempBook[1], tempBook[2], tempBook[3], tempBook[4], tempUrls);
 					
-					bookList.add(b);
-				} 
-			} 
-			//return(bookList);
+				bookList.add(b);
+			}
 		} catch (Exception e) { 
 			e.printStackTrace(); 
-			//return null;
 		} 
 	}
 
 	public static void add_to_db(ArrayList<Book> bookList) {
-		try  {
-			// Create connection
-			Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
-			Statement statement = con.createStatement();
-
-			String sql;
+		String sql = "INSERT IGNORE INTO book VALUES (?, NULL, ?, ?, ?, ?)";
+		String temp;
+		try (Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
+		PreparedStatement statement = con.prepareStatement(sql)) {
+			int count = 0;
 			for (Book book : bookList) {
 				// Insert values into database
-				sql = "INSERT INTO book VALUES ('" + book.getIsbn() + "', NULL, '" + book.getTitle() + "', '" + book.getAuthor() + "', " + book.getYear() + ", '" + book.getPublisher() + "')";
-				System.out.println(sql);
-				statement.executeUpdate(sql);
+				temp = "INSERT INTO book VALUES ('" + book.getIsbn() + "', NULL, '" + book.getTitle() + "', '" + book.getAuthor() + "', " + book.getYear() + ", '" + book.getPublisher() + "')";
+				System.out.println(count + " " + temp);
+				statement.setString(1, book.getIsbn()); statement.setString(2, book.getTitle());
+				statement.setString(3, book.getAuthor()); statement.setInt(4, Integer.valueOf(book.getYear())); 
+				statement.setString(5, book.getPublisher());
+				statement.executeUpdate();
+				count++;
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
