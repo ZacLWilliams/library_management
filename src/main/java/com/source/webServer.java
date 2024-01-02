9c174ef45e31a3ef6f2fad7a7d0259848f111730
zac <zacwilliams11@hotmail.com>
2024-01-02 23:15:45 +1100

    Changed web server to handle exception via multithreading so server no long stops when connection is cut.

---
+++ src/main/java/com/source/webServer.java
@@ -0,0 +1,77 @@
+package com.source;
+
+import java.net.*;
+import java.io.*;
+//import java.util.*;
+
+//Relocate to new source file
+public class webServer {
+    public class ClientHandler implements Runnable {
+        BufferedReader reader;
+        Socket sock;
+        public ClientHandler(Socket clientSocket) {
+            try {
+                sock = clientSocket;
+                InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+                reader = new BufferedReader(streamReader);
+            } catch (Exception ex) {ex.printStackTrace();System.out.println("TEST1");}
+        }
+
+        public void run() {
+            String line;
+            try {
+                StringBuilder request = new StringBuilder();
+                line = reader.readLine();
+                
+                while (!line.isBlank()) {
+                    //System.out.println("debug");
+                    request.append(line + "\r\n");
+                    line = reader.readLine();
+                }
+                printRequest(request);
+
+                //System.out.println("--REQUEST--");
+                //System.out.println(request);
+
+            } catch (Exception ex) {ex.printStackTrace();System.out.println("TEST2");}
+        }
+    }
+    public static void main(String[] args) {
+        webServer server = new webServer();
+        server.go();
+    }
+
+    public void printRequest(StringBuilder request) {
+        System.out.println("--REQUEST--");
+        System.out.println(request);
+    }
+
+    public void go() {
+        // Make server socket
+        try(ServerSocket serverSocket = new ServerSocket(4242)) {
+            // Socket sock = new Socket("87.121.93.116", 4242);
+            // Handle new messages
+            while(true) {
+                // Handle new incoming messages
+                try(Socket client = serverSocket.accept()) {
+                    //System.out.println("Debug:" + client.toString());
+
+                    Thread t = new Thread(new ClientHandler(client));
+                    t.start();
+
+                    OutputStream clientOutput = client.getOutputStream();
+                    clientOutput.write(("HTTP/1.1 200 OK\r\n").getBytes());
+                    clientOutput.write(("\r\n").getBytes());
+                    clientOutput.write(("Hello World").getBytes());
+
+                    clientOutput.flush();
+                    
+                    client.close();
+                }
+            }
+        } catch(IOException ex) {
+            ex.printStackTrace();
+            System.out.println("TEST3");
+        }
+    }
+}
