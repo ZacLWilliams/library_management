77d45f2c6edaebb7347c1065f8f753e2e9c453af
zac <zacwilliams11@hotmail.com>
2024-01-02 20:24:25 +1100

    Basic web server implementation

--- src/main/java/com/source/dataServer.java
+++ src/main/java/com/source/dataServer.java
@@ -10,10 +10,39 @@ public class dataServer {
         server.go();
     }
     public void go() {
-        try(ServerSocket serverSock = new ServerSocket(4242)) {
-            //Socket sock = new Socket("87.121.93.116", 4242);
+        // Make server socket
+        try(ServerSocket serverSocket = new ServerSocket(4242)) {
+            // Socket sock = new Socket("87.121.93.116", 4242);
+            // Handle new messages
             while(true) {
-                Socket sock = serverSock.accept();
+                // Handle new incoming messages
+                try(Socket client = serverSocket.accept()) {
+                    System.out.println("Debug:" + client.toString());
+
+                    // Read request
+                    InputStreamReader streamReader = new InputStreamReader(client.getInputStream());
+
+                    BufferedReader reader = new BufferedReader(streamReader);
+
+                    StringBuilder request = new StringBuilder();
+                    String userInput = reader.readLine();
+
+                    while (!userInput.isBlank()) {
+                        request.append(userInput + "\r\n");
+                        userInput = reader.readLine();
+                    }
+
+                    System.out.println("--REQUEST--");
+                    System.out.println(request);
+
+                    OutputStream clientOutput = client.getOutputStream();
+                    clientOutput.write(("HTTP/1.1 200 OK\r\n").getBytes());
+                    clientOutput.write(("\r\n").getBytes());
+                    clientOutput.write(("Hello World").getBytes());
+                    clientOutput.flush();
+
+                    client.close();
+                }
             }
         } catch(IOException ex) {
             ex.printStackTrace();
