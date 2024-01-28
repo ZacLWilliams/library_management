e05c6166cfaddc0213542f6096d99caf0a13d871
zac <zacwilliams11@hotmail.com>
2024-01-28 15:53:09 +1100

    Changed SQL command to properly search for book title and author.

--- src/main/java/com/source/checkBook.java
+++ src/main/java/com/source/checkBook.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 public class checkBook {
 	private static final String DB_URL = "jdbc:mysql://localhost/library_db";
 	private static final String USER = "root";
-	private static final String PASS = "Sapiens789-";
+	private static final String PASS = "";
 
     public static int getBooks(String search) {
         String sql = "SELECT user_id FROM user WHERE username = ? AND password = ?";
@@ -32,7 +32,18 @@ public class checkBook {
 		}
         return 0;
     } 
-    public static ArrayList<String> processWords(String search) {
+    public static String processWords(String search) {
+        int i;
+        String command = "";
+        while ((i = search.indexOf("+")) != -1) {
+            command = command + "(CONCAT(title, author) LIKE '%" + search.substring(0, i) + "%') AND ";
+            search = search.substring(i+1, search.length());
+        }
+        command = command + "(CONCAT(title, author) LIKE '%" + search + "%')";
+        System.out.println(command);
+        return command;
+    } 
+    public static ArrayList<String> temp(String search) {
         int i;
         ArrayList<String> words = new ArrayList<String>();
         while ((i = search.indexOf("+")) != -1) {
@@ -40,10 +51,6 @@ public class checkBook {
             search = search.substring(i+1, search.length());
         }
         words.add(search);
-
-        for(int z = 0; z < words.size(); z++) {
-            System.out.println(words.get(z));
-        }
         return words;
     } 
 }
--- src/main/java/com/source/test.java
+++ src/main/java/com/source/test.java
@@ -1,21 +1,16 @@
 package com.source;
 
-import java.util.ArrayList;
-
 
 public class test {
     public static void main(String[] args) {
         String search = "test+testtest+test1";
         int i;
-        ArrayList<String> words = new ArrayList<String>();
-        //System.out.println(i = search.indexOf("+"));
+        String command = "";
         while ((i = search.indexOf("+")) != -1) {
-            words.add(search.substring(0, i));
+            command = command + "(CONCAT(title, author) LIKE '%" + search.substring(0, i) + "%') AND ";
             search = search.substring(i+1, search.length());
         }
-        words.add(search);
-        for(int z = 0; z < words.size(); z++) {
-            System.out.println(words.get(z));
-        }
+        command = command + "(CONCAT(title, author) LIKE '%" + search + "%')";
+        System.out.println(command);
     }
 }
